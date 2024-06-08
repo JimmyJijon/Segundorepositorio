a51673e5c4d3bcfe725031e8beb982cc336efef7
JimmyJijon <jajijon2@tes.edu.ec>
2024-06-07 23:47:10 -0500

    agregue el ciclo while

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String args[]){
       System.out.println("hola mundo");
+
+      int i = 0;
+      while(i<10){
+        System.out.print(i + ", ");
+        i++;
+      }
+      
+
+    
     }
 }
